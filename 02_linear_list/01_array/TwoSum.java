b4cf7f09116aab83e0496ada17d7f56d464e492a
Jevin Zhao <JevinZhao@gmail.com>
2021-09-23 23:27:37 +0800

    two-sum

--- 02_linear_list/01_array/TwoSum.java
+++ 02_linear_list/01_array/TwoSum.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 /**
  * 1.两数之和
  * 给定一个整数数组 nums和一个整数目标值 target，请你在该数组中找出 和为目标值 target 的那两个整数，并返回它们的数组下标。
@@ -12,7 +14,8 @@ public class TwoSum {
      * 自己写  暴力枚举法，主要思路是：取数组中的元素与其他元素进行相加，判断两数之和是否为目标值
      * 时间复杂度： 该方法中有嵌套循环，时间复杂度为O(n^2)
      * 空间复杂度：O(1),只用到一个ints数组
-     * @param nums 整数数组
+     *
+     * @param nums   整数数组
      * @param target 目标值
      * @return 两个整数
      */
@@ -33,25 +36,29 @@ public class TwoSum {
     }
 
     /**
-     * 哈希表法，空间换时间，一次遍历数组，将不满足条件的元素存入Map中，元素值为key，下标为value
+     * 哈希表法，空间换时间，一次遍历数组，每遍历一个元素都计算待匹配的值，判断Map中是否存在待匹配的值，
+     * 不满足条件的元素存入Map中，元素值为key，下标为value，满足条件的直接输出两个键值对的value
+     * 时间复杂度：一次遍历，时间复杂度为O(n)
+     * 空间复杂度：O(n),最坏情况下要存n个键值对
      *
-     * @param nums 整数数组
+     * @param nums   整数数组
      * @param target 目标值
      * @return 两个整数
      */
     public int[] twoSum2(int[] nums, int target) {
-        //结果数组初始化
-        int[] ints = new int[2];
+        //存数据的Map
+        HashMap<Integer, Integer> hashMap = new HashMap<>(nums.length - 1);
         //遍历数组
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    //数组赋值
-                    ints[0] = i;
-                    ints[1] = j;
-                }
+            //计算下标为i的元素的partner
+            int another = target - nums[i];
+            //判断map中是否存在another，存在就输出下标
+            if (hashMap.containsKey(another)) {
+                return new int[]{i, hashMap.get(another)};
             }
+            //不存在就将该元素存入Map中
+            hashMap.put(nums[i], i);
         }
-        return ints;
+        return new int[]{0};
     }
 }
