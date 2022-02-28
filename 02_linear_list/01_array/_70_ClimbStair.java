fe182259380a9a796f1b768796809da52c764f04
Jevin Zhao <JevinZhao@gmail.com>
2022-02-28 22:49:51 +0800

    20210228_1

--- 02_linear_list/01_array/_70_ClimbStair.java
+++ 02_linear_list/01_array/_70_ClimbStair.java
@@ -21,4 +21,43 @@
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class _70_ClimbStair {
+    /**
+     * 递归法，f(x)=f(x−1)+f(x−2),该方法结果超时
+     * @param n
+     * @return
+     */
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n==2){
+            return 2;
+        }
+        return climbStairs(n-1)+climbStairs(n-2);
+    }
+
+    /**
+     * 递归优化后的解法，主要是将中间结果存储起来，避免重复计算
+     * @param n
+     * @return
+     */
+    public int climbStairs_1(int n) {
+        int[] temp=new int[n+1];
+        return climbStairs1(n,temp);
+    }
+
+    private int climbStairs1(int n, int[] temp) {
+        //递归到n时，若数组中存在，说明已经计算过，直接返回
+        if(temp[n]>0){
+            return temp[n];
+        }
+        if(n==1){
+            temp[n]=1;
+        }else if (n==2){
+            temp[n]=2;
+        }else {
+            temp[n]=climbStairs1(n-1,temp)+climbStairs1(n-2,temp);
+        }
+        return temp[n];
+    }
 }
